9c6c1b7ee3dc51b8870396e4b8013fa766c74b24
brendan <alber123@mail.chapman.edu>
2018-03-11 16:10:34 -0700

    Added comments to the various functions in the Staff Manager frame.

--- thinkgeektkinter.py
+++ thinkgeektkinter.py
@@ -41,69 +41,183 @@ managers = ["Kathryn", "Gabriela"]
 
 
 def view_employees():
+    """
+    This function is called when the user clicks the View Employees button.
+    The staff menu is removed from the grid using .grid_forget().
+    All of the names in the Listbox are removed using .delete().
+    A for loop is used to insert all of the employee staff into the Listbox.
+    The bind function is called on the Listbox.  We give it two parameters, the first argument listens for double clicks
+        on any element in the Listbox, the second argument calls the on_emp_dbl(e) method.  This is short for
+        on employee double click, and we pass it the employee who was clicked on.  This will open a pop-up window
+        with that employee's information.
+
+    The last four lines are used to display a label which identifies for the user which type of staff he or
+        she is looking at in the listbox.
+    """
     staff_menu.grid_forget()
     lb_tasks.delete(0, END)
     for employee in staff:
         lb_tasks.insert(END, employee)
+    lb_tasks.bind('<Double-1>', lambda e: on_emp_dbl(e))
+
+    type = StringVar()
+    staff_type = Label(tab1, textvariable=type)
+    type.set('Employees:')
+    staff_type.grid(column=2, row=7, sticky='ew')
 
 
 def view_managers():
+    """
+        This function is called when the user clicks the View Managers button.
+        The staff menu is removed from the grid using .grid_forget().
+        All of the names in the listbox are removed using .delete().
+        A for loop is used to insert all of the manager staff into the listbox.
+        The bind function is called on the listbox.  We give it two parameters, the first argument listens for double clicks
+            on any element in the listbox, the second argument calls the on_mgr_dbl(e) method.  This is short for
+            on manager double click, and we pass it the manager who was clicked on.  This will open a pop-up window
+            with that managers information.
+
+        The last four lines are used to display a label which identifies for the user which type of staff he or
+            she is looking at in the listbox.
+    """
     staff_menu.grid_forget()
     lb_tasks.delete(0, END)
     for employee in managers:
         lb_tasks.insert(END, employee)
+    lb_tasks.bind('<Double-1>', lambda e: on_mgr_dbl(e))
+
+    type = StringVar()
+    staff_type = Label(tab1, textvariable=type)
+    type.set('Managers:')
+    staff_type.grid(column=2, row=7, sticky='ew')
 
 
 def del_employee():
-
+    """
+    This function is called when the user clicks the delete employee button.
+    It works by deleting the selected employee from the listbox.
+    """
     employee = lb_tasks.curselection()
     lb_tasks.delete(employee)
     del staff[employee[0]]
 
 
 def add_employee():
+    """
+    This function is called when the user adds a new employees name to the txt_input Entry widget.
+    If the entry is not blank then the provided name is added as an employee.
+    """
     employee = txt_input.get()
-    if employee.strip() != '':
+    if employee.strip() != '' and employee.strip() != 'enter name here...':
         lb_tasks.insert(END, employee)
         staff.append(employee)
-        txt_input.delete(0,END)
-    else:
-        lb_tasks.insert(END, "\"blank\"")
+        txt_input.delete(0, END)
+
+
+def del_manager():
+    """
+    del_manager() is the function which is called when the user selects a manager to delete and clicks the
+    delete manager button.
+    .curselection() returns a tuple with the first element in the tuple being an integer representing the location
+    in the listbox of the selected manager.
+    The chosen manager is deleted from the listbox and then deleted from the manager list.
+    """
+    mgr = lb_tasks.curselection()
+    lb_tasks.delete(mgr)
+    del managers[mgr[0]]
+
+
+def add_manager():
+    """
+    The add_manager() function is called when the user enters a name into the Entry widget and presses the add
+    manager button.  If the Entry widget is blank or left untouched, nothing happens.
+    """
+    mgr = txt_input.get()
+    if mgr.strip() != '' and mgr.strip() != 'enter name here...':
+        lb_tasks.insert(END, mgr)
+        managers.append(mgr)
+        txt_input.delete(0, END)
 
 
 def edit_input():
+    """
     # this is a helper function for deleting the hint text in the entry bar
     # and the color of the text changes from gray to black
+    """
     txt_input.delete(0, END)
     txt_input.config(fg='black')
 
 
-def add_staff_member():
+def staff_edit_menu():
+    """
+    The staff_edit_menu() function is called when the user clicks the edit staff menu button.
+    A frame appears with two radio buttons and an entry widget.  If the manager radiobutton
+    is selected then the user can add or remove a manager using the respective buttons.
+    The user must click to select the employee/manager she wishes to delete.
+    A name must be provided in the entry widget to add an employee/manager.
 
+    """
     global txt_input
     global staff_menu
 
     if staff_menu is None:
         staff_menu = staff_menu
     else:
-        staff_menu.grid(column=2, row=0)
+        staff_menu.grid(column=2, row=1)
 
     txt_input = Entry(staff_menu, width=15, fg='gray')
     txt_input.insert(0, 'enter name here...')
     txt_input.bind('<FocusIn>', lambda x: edit_input())
-    txt_input.grid(column=0, row=1)
+    txt_input.grid(column=0, row=2)
 
+    btn_choice = StringVar()
+    emp_btn = Radiobutton(staff_menu, text='Employee', variable=btn_choice, value=0, command=use_employee_btns)
+    emp_btn.grid(column=0, row=0, sticky='w')
+    mgr_btn = Radiobutton(staff_menu, text='Manager', variable=btn_choice, value=1, command=use_mgr_btns)
+    mgr_btn.grid(column=0, row=1, sticky='w')
+
+
+def use_mgr_btns():
+    """
+    The use_mgr_btns() function is called when the Manager radio button is selected.
+    This allows the appropriate add/delete manager buttons to appear.
+    """
+    lb_tasks.delete(0, END)
+    for employee in managers:
+        lb_tasks.insert(END, employee)
+    btn_add = Button(staff_menu, text="Add Manager", command=add_manager)
+    btn_add.grid(column=0, row=3, sticky='ew')
+    del_btn = Button(staff_menu, text="Delete Manager", command=del_manager)
+    del_btn.grid(column=0, row=4, sticky='ew')
+    lb_tasks.bind('<Double-1>', lambda e: on_mgr_dbl(e))
+
+    type = StringVar()
+    staff_type = Label(tab1, textvariable=type)
+    type.set('Managers:')
+    staff_type.grid(column=2, row=7, sticky='ew')
+
+
+def use_employee_btns():
+    """
+        The use_employee_btns() function is called when the Employee radio button is selected.
+        This allows the appropriate add/delete employee buttons to appear.
+        """
+    lb_tasks.delete(0, END)
+    for employee in staff:
+        lb_tasks.insert(END, employee)
     btn_add = Button(staff_menu, text="Add Employee", command=add_employee)
-    btn_add.grid(column=0, row=2)
-
+    btn_add.grid(column=0, row=3, sticky='ew')
     del_btn = Button(staff_menu, text="Delete Employee", command=del_employee)
-    del_btn.grid(column=0, row=3)
+    del_btn.grid(column=0, row=4, sticky='ew')
+    lb_tasks.bind('<Double-1>', lambda e: on_emp_dbl(e))
+
+    type = StringVar()
+    staff_type = Label(tab1, textvariable=type)
+    type.set('Employees:')
+    staff_type.grid(column=2, row=7, sticky='ew')
 
 
-
-# lb_tasks = Listbox(tab1)
-# lb_tasks.grid(column=2, row=4)
-
+""" These are the three buttons on the left side of the Staff Manager frame."""
 
 style.configure('TButton', background=pure_apple, padding=6, relief=RAISED)
 view_employees = ttk.Button(tab1, text="View Employees", command=view_employees)
@@ -112,24 +226,46 @@ view_employees.grid(column=0, row=1)
 view_managers = ttk.Button(tab1, text="View Managers", command=view_managers)
 view_managers.grid(column=0, row=2)
 
-add_staff = ttk.Button(tab1, text="Edit Staff Menu", command=add_staff_member)
+add_staff = ttk.Button(tab1, text="Edit Staff Menu", command=staff_edit_menu)
 add_staff.grid(column=0, row=3)
 
-side_frame = LabelFrame(tab1)
+
 global txt_input
 global staff_menu
+
 staff_menu = Frame(tab1)
-side_frame.grid(column=2, columnspan=2, rowspan=5, row=1, padx=20, pady=20)
+side_frame = LabelFrame(tab1)
+side_frame.grid(column=2, columnspan=2, rowspan=5, row=2, padx=20, pady=20)
+
+type = StringVar()
+staff_type = Label(tab1, textvariable=type)
+
 lb_tasks = Listbox(side_frame)
-lb_tasks.grid(column=0, row=4)
+lb_tasks.grid(column=0, row=6, sticky='w')
+
+
+def on_emp_dbl(e):
+    widget = e.widget
+    selection = widget.curselection()
+    tl = Toplevel()
+    text = Text(tl)
+    text.insert(INSERT, staff[selection[0]])
+    text.pack()
+
+
+def on_mgr_dbl(e):
+    widget = e.widget
+    selection = widget.curselection()
+    tl = Toplevel()
+    text = Text(tl)
+    text.insert(INSERT, managers[selection[0]])
+    text.pack()
+
 
 footer_frame = LabelFrame(tab1)
 footer_frame.grid(column=1, columnspan=2, row=9, pady=20)
 made_by_label = Label(footer_frame, text="Coded with ❤️ by the fine folks @ ¯\_(ツ)_/¯", pady=10)
 made_by_label.grid(column=0, row=0)
-
-
-
 
 
 """ 
@@ -143,8 +279,6 @@ made_by_label.grid(column=0, row=0)
 
 """
 
-
-
 # TAB 2
 tab2 = ttk.Frame(tab_control)
 tab_control.add(tab2, text="Inventory Manager")
@@ -156,41 +290,4 @@ tab_control.tab(tab2, padding=10)
 
 tab_control.pack(expand=1, fill='both')
 window.mainloop()
-
-
-"""
-add_item()
-drop down menu for the various goods
-list amounts of each
-
-"""
-
-## color wheel
-## visit this website for more nice 'flat' color options
-## https://flatuicolors.com/
-#/Users/brendantyleralbert/PycharmProjects/python2/SANDBOXthinkgeektkinter.py
-
-
-
-# def get_employees():
-#
-#     tempstring = ""
-#     staff_frame = ttk.Labelframe(tab1)
-#     for emp in staff:
-#         tempstring += emp + '\n'
-#     temp_label = ttk.Label(staff_frame, text=tempstring)
-#     temp_label.grid(column=1, row=0)
-#     staff_frame.grid(column=1, row=0)
-
-# def get_managers():
-#
-#     tempstring = ""
-#     staff_frame = ttk.Labelframe(tab1)
-#     var = StringVar()
-#     for emp in managers:
-#         tempstring += emp + '\n'
-#     temp_label = ttk.Label(staff_frame, text=tempstring)
-#     temp_label.grid(column=1, row=0)
-#     staff_frame.grid(column=1, row=0)
-
 
